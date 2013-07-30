58a8d1e275cee0171ce1331acc1f1463c192466c
Will Rossiter <will.rossiter@gmail.com>
2013-07-30 21:00:57 +1200

    Rough attempt at fixing up postgres

--- lib/shipistrano/helpers/postgres.rb
+++ lib/shipistrano/helpers/postgres.rb
@@ -19,52 +19,34 @@ namespace :pgsql do
 
   desc "Uploads the local database to the remote machine"
   task :upload do
-    system "mysqldump #{mysql_database} \
-      --add-drop-table \
-      --lock-tables \
-      --extended-insert \
-      --quick \
-      -u root -ppassword > /tmp/export-local-#{mysql_database}.sql"
-
-    system "rsync -rv /tmp/export-local-#{mysql_database}.sql #{user}@#{ip}:#{deploy_to}/shared/"
+    system "pg_dump #{pgsql_database} > /tmp/export-local-#{pgsql_database}.sql"
+    system "rsync -rv /tmp/export-local-#{pgsql_database}.sql #{user}@#{ip}:#{deploy_to}/shared/"
     
     # if we want to ask the user for the remote password name then do so, 
     # otherwise we use the auto login functionality of mysql (my.cnf file)
     # at the user level
     if fetch(:pgsql_ask_for_password, true) == true then
-      _cset(:mysql_remote_password) { Capistrano::CLI.password_prompt("Enter server MySQL password: ") }  
-      
-      run "mysql -u #{mysql_user} -p#{mysql_remote_password} --execute 'CREATE DATABASE IF NOT EXISTS #{mysql_database};'" 
-      run "mysql -u #{mysql_user} -p#{mysql_remote_password} -D #{mysql_database} < #{shared_path}/export-local-#{mysql_database}.sql"
+      _cset(:pgsql_remote_password) { Capistrano::CLI.password_prompt("Enter server Postgres password: ") }  
+      run "psql -U #{pgsql_user} -W #{pgsql_remote_password} -d #{pgsql_database} -f #{shared_path}/export-local-#{pgsql_database}.sql"
     else
-      run "mysql -u #{mysql_user} --execute 'CREATE DATABASE IF NOT EXISTS #{mysql_database};'"
-      run "mysql -u #{mysql_user} -D #{mysql_database} < #{shared_path}/export-local-#{mysql_database}.sql"
+      run "psql -U #{mysql_user} -d #{pgsql_database} -f #{shared_path}/export-local-#{pgsql_database}.sql"
     end
     
-    run "rm -rf #{shared_path}/export-local-#{mysql_database}.sql"
+    run "rm -rf #{shared_path}/export-local-#{pgsql_database}.sql"
   end
   
   desc "Copys the staging database {database_name} to {database_name}_prod if \
   required. Such as when a site has the production module installed"
   task :publish do
     run "rm -rf #{shared_path}/exported_stage_database.sql"
-    run "mysqldump --defaults-file=/home/#{user}/.my.cnf \
-      #{mysql_database} \
-      --add-drop-table \
-      --lock-tables \
-      --extended-insert --quick  > #{shared_path}/exported_stage_database.sql"
+    run "pg_dump -d #{pgsql_database} -f #{shared_path}/exported_stage_database.sql"
       
     run "rm -rf #{shared_path}/exported_prod_database.sql"
-    run "mysql -u #{mysql_user} --execute 'CREATE DATABASE IF NOT EXISTS #{mysql_database}_prod'"
-    run "mysqldump --defaults-file=/home/#{user}/.my.cnf \
-      #{mysql_database}_prod \
-      --add-drop-table \
-      --lock-tables \
-      --extended-insert --quick  > #{shared_path}/exported_prod_database.sql"
+    run "pg_dump -d #{pgsql_database}_prod -f #{shared_path}/exported_prod_database.sql"
                
-    run "mysql -u #{mysql_user} -D #{mysql_database}_prod < #{shared_path}/exported_stage_database.sql"
+    run "psql -d #{pgsql_database}_prod -f #{shared_path}/exported_stage_database.sql"
   end
 end
 
-before('postgres:upload', 'core:fix_permissions')
-before('postgres:publish', 'core:fix_permissions')
+before('pgsql:upload', 'core:fix_permissions')
+before('pgsql:publish', 'core:fix_permissions')
