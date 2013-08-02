6cd83904b29b1548d70214bd58214aa853c57261
Will Rossiter <will.rossiter@gmail.com>
2013-08-03 11:32:59 +1200

    Cleanup

--- lib/shipistrano/helpers/php.rb
+++ lib/shipistrano/helpers/php.rb
@@ -1,17 +1,30 @@
 #
 # DNA Shipistrano
 #
-# PHP - Provides helper functions for dealing with PHP on servers.
+# = PHP
 #
-# Copyright (c) 2013, DNA Designed Communications Limited
-# All rights reserved.
-
+# Provides helper functions for dealing with PHP on servers.
+#
+# == Variable Exports
+#
+# - *php_bin* 
+#
+# == Task Exports
+#
+# - *php:info*
+#
+# == Todo
+#
+# (nil)
+#
 namespace :php do
-	
+  set :php_bin, "php"
+
   #
   # Short cut to check the PHP version on the remote machine.
   #
   desc "Runs a PHP info on the remote server"
-  task :php_info do
+  task :info do
     run "php -i"
   end
+end
