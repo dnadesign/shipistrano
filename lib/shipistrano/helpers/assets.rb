80c7e28cff34499c7492a49a4d4f5b4dcc3fff1f
Will Rossiter <will.rossiter@gmail.com>
2013-08-05 21:38:50 +1200

    Task description formatting

--- lib/shipistrano/helpers/assets.rb
+++ lib/shipistrano/helpers/assets.rb
@@ -26,6 +26,50 @@
 #
 
 namespace :assets do
+
+  desc <<-DESC
+    Symlinks the assets from the shared folder to the latest release.
+
+  DESC
+  task :symlink do
+    # Remove the existing list
+    run "rm -rf #{latest_release}/#{asset_location}"
+
+    # if the assets folder doesn't exist on the remote, add it
+    run "mkdir -p #{shared_path}/#{assets_folder}"
+    run "ln -nfs #{shared_path}/#{assets_folder} #{latest_release}/#{asset_location}"
+
+    assets.fix_permissions
+	end
+
+  desc <<-DESC
+    Uploads the assets from the dev copy to the remote server.
+
+  DESC
+  task :upload do
+    system "rsync -rv #{asset_location}/ #{user}@#{ip}:#{deploy_to}/shared/#{assets_folder}"
+
+    assets.fix_permissions
+  end
+
+  desc <<-DESC
+    Downloads the asset folder from the remote to the local server
+  
+  DESC
+  task :download do
+    system "rsync -rv #{user}@#{ip}:#{shared_path}/#{assets_folder}/ #{assets_folder}"
+    system "chmod -R 775 assets/"
+  end
+
+  desc <<-DESC
+    Fix the permissions on the assets folder
+
+  DESC
+  task :fix_permissions do
+    run "#{try_sudo} mkdir -p #{shared_path}/#{assets_folder}"
+    run "#{try_sudo} chmod -R 775 #{shared_path}/#{assets_folder}"
+  end
+
   #
   # Returns the asset folder location. Assumes location is the same locally and
   # remotely
@@ -38,53 +82,6 @@ namespace :assets do
     return asset_location = "#{assets_folder}"
   end
 
-  # 
-  # Returns the path to the assets folder 
-  #
-  # Asset folder should be stored in the shared path and symlinked in when we 
-  # need it.
-  #
-  desc "Symlinks the assets from the shared folder to the latest release"
-  task :symlink do
-    # Remove the existing list
-    run "rm -rf #{latest_release}/#{asset_location}"
-
-    # if the assets folder doesn't exist on the remote, add it
-    run "mkdir -p #{shared_path}/#{assets_folder}"
-    run "ln -nfs #{shared_path}/#{assets_folder} #{latest_release}/#{asset_location}"
-
-    assets.fix_permissions
-	end
-
-  #
-  # Uploads the local assets folder up to the shared assets folder.
-  #
-  desc "Uploads the assets from the dev copy to the remote server."
-  task :upload do
-    system "rsync -rv #{asset_location}/ #{user}@#{ip}:#{deploy_to}/shared/#{assets_folder}"
-
-    assets.fix_permissions
-  end
-
-  #
-  # Downloads the assets folder from the host to the local dev machine
-  #
-  desc "Downloads the asset folder from the server to the local directory"
-  task :download do
-    system "rsync -rv #{user}@#{ip}:#{shared_path}/#{assets_folder}/ #{assets_folder}"
-    
-    # local user permissions so
-    system "chmod -R 775 assets/"
-  end
-
-  # 
-  # Fixes the permissions on the assets folder.
-  #
-  desc "Fix the permissions on the assets folder"
-  task :fix_permissions do
-    run "#{try_sudo} mkdir -p #{shared_path}/#{assets_folder}"
-    run "#{try_sudo} chmod -R 775 #{shared_path}/#{assets_folder}"
-  end
 end
 
 after('deploy:create_symlink', 'assets:symlink')
