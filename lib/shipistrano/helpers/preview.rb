c12c649bedf0dc3b145687ffa8834b0605dfe6aa
John Milmine <john.milmine@dna.co.nz>
2013-12-04 16:55:22 +1300

    got auto htaccess working, and keep releases restricted

--- lib/shipistrano/helpers/preview.rb
+++ lib/shipistrano/helpers/preview.rb
@@ -21,6 +21,11 @@ set :deploy_to, "/srv/preview.dna.co.nz/site_files/#{deploy_code}/"
 set :site_symlink, "/srv/preview.dna.co.nz/site_symlinks/#{deploy_code}"
 set :auth_user, "#{deploy_code}"
 set :auth_pass, "#{deploy_pass}"
+set :keep_releases, 2
+after('deploy:update', 'preview_setup:create_htaccess')
+after('deploy:update', 'htaccess:auth:protect')
+after('deploy:update', 'deploy:cleanup')
+
 
 namespace :preview_setup do
 
@@ -47,5 +52,70 @@ namespace :preview_setup do
   end
 
   after('deploy:create_symlink', 'preview_setup:additional_symlink')
+
+
+  def setup_htaccess_ss2()
+    create_ht = <<-HTA
+### SILVERSTRIPE START ###
+<Files *.ss>
+        Order deny,allow
+        Deny from all
+        Allow from 127.0.0.1
+</Files>
+
+<IfModule mod_rewrite.c>
+        RewriteEngine On
+        RewriteBase /#{deploy_code}
+
+        RewriteCond %{REQUEST_URI} ^(.*)$
+        RewriteCond %{REQUEST_FILENAME} !-f
+        RewriteRule .* sapphire/main.php?url=%1&%{QUERY_STRING} [L]
+
+</IfModule>
+### SILVERSTRIPE END ###
+    HTA
+
+    run "if [ -f #{latest_release}/.htaccess ]; then mv #{latest_release}/.htaccess #{latest_release}/.htaccess.normal; fi"
+    put create_ht, "#{latest_release}/.htaccess"
+  end
+
+
+  def setup_htaccess_ss3()
+    create_ht = <<-HTA
+### SILVERSTRIPE START ###
+<Files *.ss>
+        Order deny,allow
+        Deny from all
+        Allow from 127.0.0.1
+</Files>
+
+<IfModule mod_rewrite.c>
+        RewriteEngine On
+        RewriteBase /#{deploy_code}
+
+        RewriteCond %{REQUEST_URI} ^(.*)$
+        RewriteCond %{REQUEST_FILENAME} !-f
+        RewriteRule .* framework/main.php?url=%1&%{QUERY_STRING} [L]
+
+</IfModule>
+### SILVERSTRIPE END ###
+    HTA
+
+    run "if [ -f #{deploy_to}.htaccess ]; then rm #{deploy_to}.htaccess; fi"
+    put create_ht, "#{deploy_to}.htaccess"
+  end
+
+
+  desc <<-DESC
+    Delete standard .htaccess and create standard ss one
+
+  DESC
+  task :create_htaccess do
+    if is_ss2
+      setup_htaccess_ss2()
+    elsif is_ss3
+      setup_htaccess_ss3()
+    end
+  end
 
 end
