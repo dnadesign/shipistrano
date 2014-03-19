add9a0c0db48ac198a4a0de6788ca6dd9328a5f4
John Milmine <john.milmine@dna.co.nz>
2014-03-19 15:08:54 +1300

    check for sudo availability

--- lib/shipistrano/helpers/silverstripe.rb
+++ lib/shipistrano/helpers/silverstripe.rb
@@ -280,7 +280,9 @@ if(file_exists(dirname(__FILE__) . '/file2url_production.php')) {
   task :fix_owner_cache_folder do
     if fetch(:use_silverstripe_cache, false) != false then
       run "if [ -d #{latest_release}/silverstripe-cache/#{user} ]; then #{try_sudo} mv #{latest_release}/silverstripe-cache/#{user} #{latest_release}/silverstripe-cache/#{group}; fi"
-      run "#{try_sudo} chown -R #{group}:#{group} #{latest_release}/silverstripe-cache"
+      if fetch(:use_sudo, false) != false then
+        run "#{try_sudo} chown -R #{group}:#{group} #{latest_release}/silverstripe-cache"
+      end
     end
   end
 
