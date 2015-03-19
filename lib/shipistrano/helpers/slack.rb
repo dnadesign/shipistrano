1775796ded3d28aefca09c8572dd03749867a019
Jeremy Olliver <jeremy.olliver@gmail.com>
2015-03-19 16:22:40 +1300

    Fixed slack integration

--- lib/shipistrano/helpers/slack.rb
+++ lib/shipistrano/helpers/slack.rb
@@ -4,19 +4,26 @@
 # set :slack_token
 # set :stage, "production"
 
-require 'net/http'
+class ShipistranoSlack
+  require 'net/http'
+
+  def self.post_to_slack(slack_team, slack_token, slack_channel, message)
+    uri = URI(URI.encode("https://#{slack_team}.slack.com/services/hooks/slackbot?token=#{slack_token}&channel=##{slack_channel}"))
+
+    Net::HTTP.start(uri.host, uri.port, use_ssl: true) do |http|
+      http.request_post uri.request_uri, message
+    end
+  end
+
+end
 
 namespace :slack do
   task :notify do
-    uri = URI(URI.encode("https://#{slack_team}.slack.com/services/hooks/slackbot?token=#{slack_token}&channel=#{slack_channel}"))
-
     user = `whoami`.chomp.split(".").map(&:capitalize).join(' ')
     revision = `cat #{local_cache}/REVISION`.chomp
     message = "#{user} has deployed #{app} version #{revision} to #{stage}"
 
-    Net::HTTP.start(uri.host, uri.port, use_ssl: true) do |http|
-      http.request_post uri.request_uri, text
-    end
+    ShipistranoSlack.post_to_slack(slack_team, slack_token, slack_channel, message)
   end
 end
 
