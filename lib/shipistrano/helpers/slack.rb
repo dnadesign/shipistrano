cc4302d9940e238108b39e50a38d7e5306f93991
Will Rossiter <will@fullscreen.io>
2015-06-26 10:09:00 +1200

    If slack is included without a  token then ignore it.

--- lib/shipistrano/helpers/slack.rb
+++ lib/shipistrano/helpers/slack.rb
@@ -8,10 +8,12 @@ class ShipistranoSlack
   require 'net/http'
 
   def self.post_to_slack(slack_team, slack_token, slack_channel, message)
-    uri = URI(URI.encode("https://#{slack_team}.slack.com/services/hooks/slackbot?token=#{slack_token}&channel=##{slack_channel}"))
+    if slack_token?
+      uri = URI(URI.encode("https://#{slack_team}.slack.com/services/hooks/slackbot?token=#{slack_token}&channel=##{slack_channel}"))
 
-    Net::HTTP.start(uri.host, uri.port, use_ssl: true) do |http|
-      http.request_post uri.request_uri, message
+      Net::HTTP.start(uri.host, uri.port, use_ssl: true) do |http|
+        http.request_post uri.request_uri, message
+      end
     end
   end
 
@@ -19,19 +21,23 @@ end
 
 namespace :slack do
   task :prenotify do
-    user = `whoami`.chomp.split(".").map(&:capitalize).join(' ')
-    revision = `cat #{local_cache}/REVISION`.chomp
-    message = "#{user} is deploying #{app} version #{revision} to #{stage}"
+    if fetch(:slack_token, false) then
+      user = `whoami`.chomp.split(".").map(&:capitalize).join(' ')
+      revision = `cat #{local_cache}/REVISION`.chomp
+      message = "#{user} is deploying #{app} version #{revision} to #{stage}"
 
-    ShipistranoSlack.post_to_slack(slack_team, slack_token, slack_channel, message)
+      ShipistranoSlack.post_to_slack(slack_team, slack_token, slack_channel, message)
+    end
   end
 
   task :postnotify do
-    user = `whoami`.chomp.split(".").map(&:capitalize).join(' ')
-    revision = `cat #{local_cache}/REVISION`.chomp
-    message = "#{user} has deployed #{app} version #{revision} to #{stage}"
+    if fetch(:slack_token, false) then
+      user = `whoami`.chomp.split(".").map(&:capitalize).join(' ')
+      revision = `cat #{local_cache}/REVISION`.chomp
+      message = "#{user} has deployed #{app} version #{revision} to #{stage}"
 
-    ShipistranoSlack.post_to_slack(slack_team, slack_token, slack_channel, message)
+      ShipistranoSlack.post_to_slack(slack_team, slack_token, slack_channel, message)
+    end
   end
 end
 
